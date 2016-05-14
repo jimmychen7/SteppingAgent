08f8aec4f11922876d3b51db223de9240fb20324
WhalesAreDelicious <weilony@hotmail.com>
2016-05-14 18:26:17 +1000

    Fixed some messy code Added .gitignore

--- src/AgentMap.java
+++ src/AgentMap.java
@@ -1,10 +1,11 @@
 
 public class AgentMap {
+	public static final int mapSize = 160;
 	
 	public AgentMap() {
-		agentMap = new AgentElement[159][159];
-		for(int i = 0; i < 159; i++)
-			for(int j = 0; j < 159; j++)
+		agentMap = new AgentElement[mapSize][mapSize];
+		for(int i = 0; i < mapSize; i++)
+			for(int j = 0; j < mapSize; j++)
 				agentMap[j][i] = new AgentElement("unknown");
 	}
 	
