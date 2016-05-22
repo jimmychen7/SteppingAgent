b9829be4e839bb0880ab37dfbaf4094ee20c849d
WhalesAreDelicious <weilony@hotmail.com>
2016-05-22 17:39:04 +1000

    Renamed AgentMap to AgentState

--- src/AgentState.java
+++ src/AgentState.java
@@ -1,8 +1,16 @@
 import java.util.LinkedList;
-
-public class AgentMap {
-	
-	public AgentMap() {
+/**
+ * 
+ * @author Jimmy Chen, Weilon Ying
+ * AgentState class
+ * Tracks the agent's current state, including location, inventory (tools and items) and a 
+ * map of all visited/seen nodes
+ */
+public class AgentState {
+	/**
+	 * Constructor for AgentMap. Initialises the map 
+	 */
+	public AgentState() {
 		agentMap = new AgentElement[159][159];
 		for(int i = 0; i < 159; i++) 
 			for(int j = 0; j < 159; j++)
@@ -51,11 +59,22 @@ public class AgentMap {
         updateVisited(xCurrPos, yCurrPos);
 	}
 	
+	/**
+	 * Method that determines whether a certain coordinate is a valid map coordinate or not
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 * @return True if coordinate is a valid map coordinate, and false otherwise
+	 */
 	boolean withinMap(int x, int y) {
 		if(x >=0 && x < 160 && y >= 0 && y < 160) return true;
 		return false;
 	}
 	
+	/**
+	 * Method that updates coordinates to be reachable as the agent moves into new areas
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
 	private void updateReachable(int x, int y) {
 		//set currNode and neigbours as reachable = true.
 		agentMap[y][x].setReachable(true);
@@ -64,11 +83,20 @@ public class AgentMap {
 		if(withinMap(y, x+1)) agentMap[y][x-1].setReachable(true);
 		if(withinMap(y, x-1)) agentMap[y][x+1].setReachable(true);
 	}
+	
+	/**
+	 * Sets agent's current location to be "visited"
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
 	private void updateVisited(int x, int y) {
 		//set currNode to visited = true.
 		agentMap[y][x].setVisited(true);
 	}
-	
+	/**
+	 * Checks agent's current location and updates inventory if the location contains an item.
+	 * The location is then updated to no longer contain that item.
+	 */
 	private void updateTools() {
 		AgentElement currElement = agentMap[currPosition.y][currPosition.x];
 		
@@ -86,15 +114,26 @@ public class AgentMap {
 			currElement.setSymbol(" ");
 		}		
 	}
-	
+	/**
+	 * Returns agent's current location
+	 * @return agent's current location
+	 */
 	public Coordinate getCurrPosition() {
 		return this.currPosition.clone();
 	}
-	
+	 /**
+	  * Returns agent's current inventory (tools)
+	  * @return agent's current inventory (tools)
+	  */
 	ToolBox getTools() {
 		return this.tools;
 	}
 	
+	/**
+	 * Method that determines the next location (node) to visit. This is determined by whether the agent
+	 * has gold or not, proximity to items of interest, known obstacles and locations that have not been visited yet.
+	 * @return The coordinate of the next location (node) to visit
+	 */
 	public Coordinate getNodeToVisit() {
 	    if (getTools().isHasGold()) {
 	        return new Coordinate(79, 79); //set goal to home if agent possesses gold
@@ -154,22 +193,6 @@ public class AgentMap {
 				return keyPos;
 			}
 			if (goal == Landmark.STONE) {
-				//Get closest stone and return it
-				/*Coordinate closestStonePos;
-				int closestStoneDist;
-				
-				closestStonePos = stonePos.getFirst();
-				Heuristic h = new ManhattanDistanceHeuristic();
-				closestStoneDist = h.getHeuristic(currPosition, closestStonePos);
-				
-				for (Coordinate compare : stonePos) {
-					int compareStoneDist = h.getHeuristic(currPosition, compare);
-					if (compareStoneDist < closestStoneDist) {
-						closestStonePos = compare;
-					}
-				}
-				
-				return closestStonePos;*/
 				return stonePos;
 			}
 			
@@ -235,13 +258,18 @@ public class AgentMap {
 			}
 		}		
 		
-		//Else make decision 
+		//Null decision. If the agent is unable to decide what to do next, a null decision occurs.
+		//This shouldn't happen and if it does, debugging should be done to determine the cause.
 		System.out.println("NULL DECISION!");
 		System.exit(-1);
 		
 		return null;
 	}
 	
+	/**
+	 * Updates the agent's map for future reference
+	 * @param view The agent's current view which is then used to add to the map
+	 */
 	public void updateMap(char[][] view) {
 		int xCurrPos = currPosition.x;
 		int yCurrPos = currPosition.y;
@@ -311,11 +339,18 @@ public class AgentMap {
         	break;
         }    
 	}
-	
+	/**
+	 * Returns the agent's current facing direction
+	 * @return the agent's current facing direction
+	 */
 	public int getDirection() {
 		return this.dirn;
 	}
 	
+	/**
+	 * Updates the agent's current facing direction
+	 * @param ch character represents the direction the agent faced (left/right)
+	 */
 	void updateDirection(char ch) {
 		if(( ch == 'L' )||( ch == 'l' )) {
 			dirn = ( dirn + 1 ) % 4;
@@ -324,7 +359,11 @@ public class AgentMap {
 	    	dirn = ( dirn + 3 ) % 4;
 	    }
 	}	
-	
+	/**
+	 * Method that determines whether the agent can go forward.
+	 * @param num_stones_held The number of stepping stones the agent currently has
+	 * @return True if the agent is able to move forward unhindered, and false otherwise
+	 */
 	public boolean canGoForward(int num_stones_held) {
 		
 		int xPos = currPosition.x;
@@ -344,7 +383,9 @@ public class AgentMap {
 		return false;
 	}
 	
-	
+	/**
+	 * Prints out the agent's map
+	 */
 	public void print() {
         String name;
         int i,j;
@@ -357,25 +398,26 @@ public class AgentMap {
                 
             
                 switch(agentMap[i][j].getSymbol()) {
-                    case ".":
-                    case " ":
-                    case "T":
-                    case "-":
-                    case "~":
-                    case "*":
-                    case "a":
-                    case "k":
-                    case "o":
-                    case "O":
-                    case "g":
+                    case UNKNOWN:
+                    case EMPTY:
+                    case TREE:
+                    case DOOR:
+                    case WATER:
+                    case WALL:
+                    case AXE:
+                    case KEY:
+                    case STONE_OBJECT:
+                    case STONE_PLACED:
+                    case GOLD:
                         add = agentMap[i][j].getSymbol();
                         break;
                     default: 
-                        System.out.print("?");
+                        System.out.print("?"); //If none of the possible symbols, print out "?". This should never happen and should be
+                                                //debugged if it does
                         break;
                 }
                 
-                if (!add.equals(".")) {
+                if (!add.equals(UNKNOWN)) {
                     printLine = true; //only print this line if there is anything except unknown char
                 }
                 if (currPosition.x == j && currPosition.y == i) {
@@ -390,20 +432,38 @@ public class AgentMap {
         }
             
     }
+	
+	/**
+	 * Returns the agent's map as a matrix of AgentElements
+	 * @return
+	 */
 	public AgentElement[][] getAgentElements() {
 		return agentMap;
 	}
-	
-	
     	
-	private AgentElement[][] agentMap;
-	private Coordinate currPosition;
-	private ToolBox tools; 
-	private int dirn;
+	private AgentElement[][] agentMap; //The agent's map
+	private Coordinate currPosition; //Agent's current position
+	private ToolBox tools;  //Agent's current inventory (tools)
+	private int dirn; //Agent's current facing direction
+	
+	//Cardinal directions
 	final static int EAST   = 0;
     final static int NORTH  = 1;
     final static int WEST   = 2;
     final static int SOUTH  = 3;  
+    
+    //Possible map element symbols
+    public final static String UNKNOWN      = AgentElement.UNKNOWN;
+    public final static String EMPTY        = AgentElement.EMPTY;
+    public final static String TREE         = AgentElement.TREE;
+    public final static String DOOR         = AgentElement.DOOR;
+    public final static String WATER        = AgentElement.WATER;
+    public final static String WALL         = AgentElement.WALL;
+    public final static String AXE          = AgentElement.AXE;
+    public final static String KEY          = AgentElement.KEY;
+    public final static String STONE_OBJECT = AgentElement.STONE_OBJECT;
+    public final static String STONE_PLACED = AgentElement.STONE_PLACED;
+    public final static String GOLD         = AgentElement.GOLD;
     
     
 }
